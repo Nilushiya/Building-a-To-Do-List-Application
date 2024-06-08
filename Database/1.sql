3f25a92204cc740de584a8b2b9aaedef949640d5
Nilushiya <nilushiyak@gmail.com>
2024-06-08 14:50:17 +0530

    database add

---
+++ Database/1.sql
@@ -0,0 +1,4 @@
+id,user_name,user_type,is_active,email,password,created_at
+1,Nilushiya,USER,1,nilushiyak@gmail.com,$2b$10$D7WegrOKHqcWf2bBe8Ip1eDZ28zhcOJjmH.qp17FQi9AUDFIF41Q2,"2024-06-07 22:36:52"
+2,Thenujan,USER,1,thenu7400@gmail.com,$2b$10$gD/B62KnBj2JcpFSMcWcduMAKNzsd7dAYNMPEin0SMLzZbfNHruYO,"2024-06-08 08:28:34"
+3,Ayan,USER,1,ayant@gmail.com,$2b$10$bThLU9z0bCr7lDNzZxv76uYAhLD/4Bqum304/PIN.jbt..JN7dc7.,"2024-06-08 08:29:12"
